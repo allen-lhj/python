087387e99d653609da9ae3c12fc1f38607ddc9e4
allen-lhj <liu772636821@163.com>
2022-07-07 08:12:51 +0800

    fastapi

--- day56-60/main.py
+++ day56-60/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,3 +13,17 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# 基于baseModel 定义请求体结构(json 对象)
+
+
+class LoginUser(BaseModel):
+    phone: str
+    code: str
+
+
+@app.post("/login")
+def user_login(user: LoginUser):
+    # 查询phone是否存在
+    # 验证code 是否有效
+    return {'msg': '用户已登录', 'phone': user.phone}
